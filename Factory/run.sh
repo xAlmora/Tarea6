2edc636b29ad9958a0e1b17a5e664f94539a7666
Alex <>
2018-03-12 12:18:01 -0600

    Agregado Metodo Factory

---
+++ Factory/run.sh
@@ -0,0 +1,6 @@
+mvn package
+java -jar target/Factory-1.0-SNAPSHOT.jar
+
+echo "*************************************"
+echo "Termindao. Por cierto, el ejemplo del Servlet visto en clase viene en: simpleServlet.tar.gz"
+echo "*************************************"
---
+++ Factory/src/main/java/mx/itam/ComputerFactory.java
@@ -0,0 +1,13 @@
+package mx.itam;
+
+public class ComputerFactory {
+
+    public static Computer getComputer(String type, String ram, String hdd, String cpu){
+        if("PC".equalsIgnoreCase(type)){
+            return new PersonalComputer(ram, hdd, cpu);
+        }else if("Server".equalsIgnoreCase(type)){
+            return new Server(ram, hdd, cpu);
+        }
+        return null;
+    }
+}
